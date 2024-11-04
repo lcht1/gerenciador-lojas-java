c61493aea3bf9bebbd75aaa591d13cf26066b697
manuela <manuleichtveis@gmail.com>
2024-11-03 21:25:33 -0300

    finish etapa 3

--- src/Etapa3/Cosmetico.java
+++ src/Etapa3/Cosmetico.java
@@ -3,21 +3,12 @@ package Etapa3;
 public class Cosmetico extends Loja {
     private double taxaComercializacao;
 
-    // Construtor que inicializa todos os atributos, incluindo os da superclasse
     public Cosmetico(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario,
                      Endereco endereco, Data dataFundacao, double taxaComercializacao) {
         super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
         this.taxaComercializacao = taxaComercializacao;
     }
 
-    // Construtor alternativo, caso a loja não tenha salário base definido
-    public Cosmetico(String nome, int quantidadeFuncionarios, Endereco endereco,
-                     Data dataFundacao, double taxaComercializacao) {
-        super(nome, quantidadeFuncionarios, endereco, dataFundacao);
-        this.taxaComercializacao = taxaComercializacao;
-    }
-
-    // Getter e Setter para taxaComercializacao
     public double getTaxaComercializacao() {
         return taxaComercializacao;
     }
@@ -26,7 +17,6 @@ public class Cosmetico extends Loja {
         this.taxaComercializacao = taxaComercializacao;
     }
 
-    // Sobrescrevendo o método toString para incluir taxaComercializacao
     @Override
     public String toString() {
         return super.toString() +
